be6bb31c88852b4c176e92c173b3d70a7808bf29
Andi Angel <andi.angel@cgi.com>
2023-06-24 13:16:23 +0300

    Add mock data for API development

---
+++ w8-tracker-be/mockData.js
@@ -0,0 +1,16 @@
+const mockData = [
+  {
+    date: '2023-06-23',
+    weight: '102.03'
+  },
+  {
+    date: '2023-06-22',
+    weight: '103'
+  },
+  {
+    date: '2023-06-24',
+    weight: '102.7'
+  },
+]
+
+module.exports = mockData;
