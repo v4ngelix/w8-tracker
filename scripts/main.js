9fbf816dd40fd484d8c1cbb4760f2aed9e8608d0
Andi Angel <andi.angel@boheemia.ee>
2024-01-05 16:31:07 +0200

    Rewrite weight adding logic

--- scripts/main.js
+++ scripts/main.js
@@ -37,49 +37,59 @@ function getWeights() {
       }
     })
     .then(data => {
-        console.log(data)
-        weightData = data;
-        const tbody = document.getElementsByTagName("tbody")[0];
-        tbody.innerHTML = "";
+      console.log(data)
+      weightData = data;
+      const tbody = document.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = "";
 
-        weightData.forEach((weight) => {
-          const row = tbody.insertRow();
-          row.insertCell(0).innerHTML = `<div class="user-selection__bubble" style="background-color: red"></div>`;
-          row.insertCell(1).innerHTML = `${Number(weight.weight).toFixed(2)} kg`;
+      weightData.forEach((weight) => {
+        const row = tbody.insertRow();
+        row.insertCell(0).innerHTML = `<div class="user-selection__bubble" style="background-color: red"></div>`;
+        row.insertCell(1).innerHTML = `${Number(weight.weight).toFixed(2)} kg`;
 
-          const date = new Date(weight.date);
-          const day = date.getDate();
-          const dayPrefix = day < 10 ? "0" : "";
-          const dayString = dayPrefix + day;
-          const month = date.getMonth() + 1;
-          const monthPrefix = month < 10 ? "0" : "";
-          const monthString = monthPrefix + month;
-          const year = String(date.getFullYear()).slice(2, 4);
-          row.insertCell(2).innerHTML = `${dayString}.${monthString}.${year}`;
-          row.insertCell(3).innerHTML = "";
-        });
+        const date = new Date(weight.date);
+        const day = date.getDate();
+        const dayPrefix = day < 10 ? "0" : "";
+        const dayString = dayPrefix + day;
+        const month = date.getMonth() + 1;
+        const monthPrefix = month < 10 ? "0" : "";
+        const monthString = monthPrefix + month;
+        const year = String(date.getFullYear()).slice(2, 4);
+        row.insertCell(2).innerHTML = `${dayString}.${monthString}.${year}`;
+        row.insertCell(3).innerHTML = "";
+      });
 
-        let index = 0;
-
-        function nextUpdateSpeed(inputIndex) {
-          const nextSpeed = (weightData.length * 2) * Math.log(inputIndex);
-          console.log({nextSpeed});
-          return nextSpeed;
+      let index = 0;
+      const updateEvent = () => setTimeout(() => {
+        const newData = weightData[index];
+        console.log({newData});
+        chart.data.labels.push(newData.date);
+        chart.data.datasets[0].data.push(newData.weight);
+        chart.update();
+        index++;
+        if (index < weightData.length) {
+          updateEvent()
         }
-        const updateEvent = () => setTimeout(() => {
-          const newData = weightData[index];
-          console.log({newData});
-          chart.data.labels.push(newData.date);
-          chart.data.datasets[0].data.push(newData.weight);
-          chart.update();
-          index++;
-          if (index < weightData.length) {
-              updateEvent()
-          }
-        }, nextUpdateSpeed(index));
+      }, 50);
 
-        updateEvent(index);
+      updateEvent(index);
     });
 }
 
 getWeights();
+
+function addWeight(e) {
+  e.preventDefault();
+  const date = document.getElementById("dateInput").value;
+  const weight = document.getElementById("weightInput").value;
+  console.log({date, weight});
+  fetch(`https://w8.boheemia.ee/api/add-weight?date=${date}&weight=${weight}`).then(response => {
+    if (response.ok) {
+      getWeights();
+      return response.json()
+    } else {
+      throw new Error("Something went wrong");
+    }
+  })
+  console.log(e);
+}
