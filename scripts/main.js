927cdc57e084a75a5035d7be8d109c0e4439859c
Andi Angel <andi.angel@boheemia.ee>
2024-03-20 08:32:58 +0200

    Fix indendation

--- scripts/main.js
+++ scripts/main.js
@@ -31,49 +31,48 @@ document.getElementById("dateInput").valueAsDate = new Date();
 function getWeights() {
   let weightData = [];
   fetch(API_URL + '/get-weights').then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error("Something went wrong");
-      }
-    })
-    .then(data => {
-      console.log(data)
-      weightData = data;
-      const tbody = document.getElementsByTagName("tbody")[0];
-      tbody.innerHTML = "";
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error("Something went wrong");
+    }
+  }).then(data => {
+    console.log(data)
+    weightData = data;
+    const tbody = document.getElementsByTagName("tbody")[0];
+    tbody.innerHTML = "";
 
-      weightData.forEach((weight) => {
-        const row = tbody.insertRow();
-        row.insertCell(0).innerHTML = `<div class="user-selection__bubble" style="background-color: red"></div>`;
-        row.insertCell(1).innerHTML = `${Number(weight.weight).toFixed(2)} kg`;
+    weightData.forEach((weight) => {
+      const row = tbody.insertRow();
+      row.insertCell(0).innerHTML = `<div class="user-selection__bubble" style="background-color: red"></div>`;
+      row.insertCell(1).innerHTML = `${Number(weight.weight).toFixed(2)} kg`;
 
-        const date = new Date(weight.date);
-        const day = date.getDate();
-        const dayPrefix = day < 10 ? "0" : "";
-        const dayString = dayPrefix + day;
-        const month = date.getMonth() + 1;
-        const monthPrefix = month < 10 ? "0" : "";
-        const monthString = monthPrefix + month;
-        const year = String(date.getFullYear()).slice(2, 4);
-        row.insertCell(2).innerHTML = `${dayString}.${monthString}.${year}`;
-        row.insertCell(3).innerHTML = "";
-      });
-
-      let index = 0;
-      const updateEvent = () => setTimeout(() => {
-        const newData = weightData[index];
-        chart.data.labels.push(newData.date);
-        chart.data.datasets[0].data.push(newData.weight);
-        chart.update();
-        index++;
-        if (index < weightData.length) {
-          updateEvent()
-        }
-      }, 50);
-
-      updateEvent(index);
+      const date = new Date(weight.date);
+      const day = date.getDate();
+      const dayPrefix = day < 10 ? "0" : "";
+      const dayString = dayPrefix + day;
+      const month = date.getMonth() + 1;
+      const monthPrefix = month < 10 ? "0" : "";
+      const monthString = monthPrefix + month;
+      const year = String(date.getFullYear()).slice(2, 4);
+      row.insertCell(2).innerHTML = `${dayString}.${monthString}.${year}`;
+      row.insertCell(3).innerHTML = "";
     });
+
+    let index = 0;
+    const updateEvent = () => setTimeout(() => {
+      const newData = weightData[index];
+      chart.data.labels.push(newData.date);
+      chart.data.datasets[0].data.push(newData.weight);
+      chart.update();
+      index++;
+      if (index < weightData.length) {
+        updateEvent()
+      }
+    }, 50);
+
+    updateEvent(index);
+  });
 }
 
 function addWeight() {
