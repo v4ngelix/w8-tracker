94c97146d51c4fae60d35e7f911900f208b72a3a
Andi Angel <andi.angel@boheemia.ee>
2024-03-24 07:52:51 +0200

    Add missing import

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const querystring = require('querystring');
 const Url  = require('url');
+const getWeights = require('./controls/getWeights');
 
 const connection = mysql.createConnection({
   host: 'd66029.mysql.zonevs.eu',
