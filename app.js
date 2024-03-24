c83676f6c6f5f67ef7c5061d3ceedfefcab6a2d4
Andi Angel <andi.angel@boheemia.ee>
2024-03-24 07:27:56 +0200

    Improve url handling

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const server = http.createServer(
     response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     const { url, method, } = request;
-    const requestPath = url.split('/').slice(1);
+    const requestPath = url.split(/[\/?]/).slice(1);
     console.log('Incoming url: ', url, requestPath, method);
 
     if (request.url === '/' && method === 'GET') {
@@ -64,7 +64,9 @@ const server = http.createServer(
       response.end();
     }
     else if (requestPath[0] === 'api') {
-      console.log('It is an API request');
+      const endPoint = requestPath[1] ?? '';
+      console.log('It is an API request', endPoint);
+
 
       if (requestPath[1].includes("addWeight") && request.method === "GET") {
         const parsedUrl = Url.parse(url);
@@ -123,9 +125,7 @@ const server = http.createServer(
     }
 
     // DELETE FROM `weights_aa` WHERE `weights_aa`.`date` = '0000-00-00';
-    else if (
-      requestPath[1].includes("getWeights") && request.method === "GET"
-    ) {
+    if (endPoint === "getWeights" && request.method === "GET") {
       connection.query('SELECT * FROM weights_aa', (error, rows) => {
       if (error) {
           response.writeHead(500, { "Content-Type": "application/json" });
