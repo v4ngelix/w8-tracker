3a656990b7eb99194e88513183780ee850ed6a29
Andi Angel <andi.angel@boheemia.ee>
2024-07-15 10:39:38 +0300

    Revert "Try fixing server crash problem"
    This reverts commit bb317e61dd4dc2d6b1eb092d2e5b5af35c90fba5.

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@
 const weightInput = document.getElementById("weightInput");
 if (weightInput) weightInput.focus();
 
-const API_URL = `${window.location.href.split('/')[2]}api`;
+const API_URL = `${window.location.href}api`;
 
 document.getElementById("dateInput").valueAsDate = new Date();
 
